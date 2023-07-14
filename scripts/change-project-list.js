d483b0a3615024aaf06e061da3ccabe7f59fe840
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-07-14 11:35:11 -0400

    Added Event Listeners to CSS3 and JS Logos

--- scripts/change-project-list.js
+++ scripts/change-project-list.js
@@ -4,15 +4,15 @@ const css3_logo = document.getElementById("css3-logo");
 const js_logo = document.getElementById("js-logo");
 const python_logo = document.getElementById("python-logo");
 
-/* Functions */
+let list_item;
+let link;
 
 /* Event Listners */
 
+/* HTML5 Event Listeners */
+
 html5_logo.addEventListener("click", () => {
   project_list.innerHTML = ""; //Removes all the content in the project_list
-
-  let list_item;
-  let link;
 
   for (let i = 0; i < 3; i++) {
     list_item = document.createElement("li");
@@ -41,11 +41,82 @@ html5_logo.addEventListener("click", () => {
   }
 });
 
-python_logo.addEventListener("click", () => {
-  project_list.innerHTML = ""; //Removes all the content in the project_list
+/* CSS3 Event Listener */
 
-  let list_item;
-  let link;
+css3_logo.addEventListener("click", () => {
+  project_list.innerHTML = "";
+
+  for (let i = 0; i < 5; i++) {
+    list_item = document.createElement("li");
+    link = document.createElement("a");
+    link.target = "_blank";
+
+    switch (i) {
+      case 0:
+        link.href = "https://github.com/jjhenrichs/linkedin-loader";
+        link.innerHTML = "LinkedIn Loader";
+        break;
+      case 1:
+        link.href = "https://github.com/jjhenrichs/my_wellness_living_loader";
+        link.innerHTML = "Wellness Rainbow Loader";
+        break;
+      case 2:
+        link.href = "https://github.com/jjhenrichs/vudu-loader";
+        link.innerHTML = "Vudu Loader";
+        break;
+      case 3:
+        link.href = "https://github.com/jjhenrichs/Doggy-Daycare";
+        link.innerHTML = "Doggy Daycare";
+        break;
+      case 4:
+        link.href = "https://github.com/jjhenrichs/tea-cozy";
+        link.innerHTML = "Tea Cozy";
+        break;
+      case 5:
+        link.href = "https://github.com/jjhenrichs/portfolio";
+        link.innerHTML = "and this Web Portfolio";
+        break;
+      default:
+        break;
+    }
+
+    list_item.appendChild(link);
+    project_list.appendChild(list_item);
+  }
+});
+
+/* JS Event Listener */
+
+js_logo.addEventListener("click", () => {
+  project_list.innerHTML = "";
+
+  for (let i = 0; i < 2; i++) {
+    list_item = document.createElement("li");
+    link = document.createElement("a");
+    link.target = "_blank";
+
+    switch (i) {
+      case 0:
+        link.href = "https://github.com/jjhenrichs/js-tictactoe";
+        link.innerHTML = "Tic-Tac-Toe";
+        break;
+      case 1:
+        link.href = "https://github.com/jjhenrichs/portfolio";
+        link.innerHTML = "and this Web Portfolio";
+        break;
+      default:
+        break;
+    }
+
+    list_item.appendChild(link);
+    project_list.appendChild(list_item);
+  }
+});
+
+/* Python Event Listener */
+
+python_logo.addEventListener("click", () => {
+  project_list.innerHTML = "";
 
   for (let i = 0; i < 2; i++) {
     list_item = document.createElement("li");
@@ -55,7 +126,7 @@ python_logo.addEventListener("click", () => {
     switch (i) {
       case 0:
         link.href = "https://github.com/jjhenrichs/py_calculator";
-        link.innerHTML = "Python Calculator";
+        link.innerHTML = "Tkinter Calculator";
         break;
       case 1:
         link.href =
